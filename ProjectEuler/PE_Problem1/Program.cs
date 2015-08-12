b0c75cdc62665e337c0e40c604e3ca034e750809
sarkunanrax <sarkunanrax@gmail.com>
2015-08-12 08:44:26 +0800

    Added missing

---
+++ ProjectEuler/PE_Problem1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PE_Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var upperBound = 1000;
+            var sum = 0;
+
+            for (var i = 0; i < upperBound; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -9,19 +9,6 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            var upperBound = 1000;
-            var sum = 0;
-
-            for (var i = 0; i < upperBound; i++) 
-            {
-                if(i % 3 == 0 || i % 5 == 0)
-                {
-                    sum += i;
-                }    
-            }
-
-            Console.WriteLine(sum);
-            Console.ReadLine();
         }
     }
 }
