144c9e6862ff769d716689a37f68249874286875
sarkunanrax <sarkunan.vijayan@infinitilab.com>
2015-08-13 17:15:13 +0800

    1 Problem 3

--- ProjectEuler/PE_Problem3/Program.cs
+++ ProjectEuler/PE_Problem3/Program.cs
@@ -3,46 +3,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/*
+ * Solution
+ * Get the factors of the number
+ *  In this case prime factors 
+ *  
+ * 1. Get a factor of the number: Use % 
+ * 2. Check that factor if its a prime : loop and check if values lower than it can divide it 
+ * 3. If is a prime, save 
+ * 4. Move on to the next number
+ */
+
 namespace PE_Problem3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var value = 600851475143;
-            //var value = 13195;
-            var primeNumbers = new List<int>();
-            var maxPrimeFactor = 0;
+            long maxPrimeFactor = 0;
+            long inputValue = 600851475143;
+            long value = inputValue;
 
-            // another thing to remember is that prime numbers are odd numbers after 2 can remove the check for isprime if wanted
-            for (var i = 3; i < value; i++)
+            // Solution 1
+            // Checking each number -- this takes too long to process ._. what to do next 
+
+            // Loop through the first value 
+            //for (var i = 2; i < value; i++) 
+            //{
+            //    if (value % i == 0) 
+            //    {
+            //        bool isPrime = true;
+
+            //        // Check if the value i is a prime number by cycling through 2 <= j < i 
+
+            //        for (var j = 2; j < i; j++)
+            //        {
+            //            if (i % j == 0)
+            //            {
+            //                isPrime = false;
+            //                break;
+            //            }
+            //        }
+            //        if (isPrime)
+            //        {
+            //            maxPrimeFactor = i;
+            //        }
+            //    }
+            //}
+
+            /*
+             * Solution 2 
+             * 
+             * Used help for this brain not strong enough.. Oh brain.. 
+             * Apparently there is this "Fundamental theorem of arithmetic" that states that a number is a prime or is a product of prime numbers 
+             * Keeping this in mind something like this can be written instead..
+             * 
+             * 1. Value divide by n ( starting from 2 ) 
+             * 2. If % has no remaining value save the results
+             * 3. Run step 1 and 2 until there is a remainder THEN
+             * 4. Move on to adding the n by 1 
+             * 5. If n reaches the value or is lower, stop the loop
+             *  5.1 if n^2 is lower than the value stop as well because that number would not be divisible anymore
+             */
+
+            var i = 2;
+            
+            while (i * i <= value)
             {
-                if (isPrimeNumber(i)) 
+                // as long as the number can be divided by i, divide it otherwise add 1
+                if(value % i == 0)
                 {
-                    var temp = (value % i == 0) ? value % i : 0;
-                    if (isPrimeNumber(temp))
-                    { 
-                        
-                    }
+                    value = value / i;
+                    maxPrimeFactor = i;
+                }
+                else
+                {
+                    i++;
                 }
             }
-
+            if (value > maxPrimeFactor)
+            {
+                maxPrimeFactor = value;
+            }
+            
+            Console.WriteLine(maxPrimeFactor);
             Console.ReadLine();
 
-        }
-
-        public static bool isPrimeNumber(long value)
-        {
-            if (value != 1 || value != 2 || value != 3) 
-            {
-                if (value % 2 == 0 || value % 3 == 0)
-                {
-                    return false;
-                }
-            }
-
-
-            return true;        
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Projects101
-Random code and learn projects. More to come soon!
+# Problems101
+Random code and problems from Euler Project and stuff. 
+
+Well they are solution for the Euler Project problems so go through them at your own risk. 
+
+Just doing this for fun.
